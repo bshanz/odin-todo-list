e49c60582a80dbd40325aa27cfbc201c8db8d475
Manu Cornet <m@ma.nu>
2022-06-14 11:50:00 +0000

    Move the logic to create the DOM for a task into the Task class. This is better for 'encapsulation' and code reuse. That way, the Task class knows how to render itself.

--- src/index.js
+++ src/index.js
@@ -68,79 +68,11 @@ form.addEventListener("submit", (e) => {
   // reset the form
   document.querySelector("form").reset();
 
-  // create elements
-  const toDoCard = document.createElement("div");
-  const toDoContent = document.createElement("div");
-  const upperCard = document.createElement("div");
-  const theTitle = document.createElement("h3");
-  const theDescription = document.createElement("p");
-  const theProject = document.createElement("p");
-  const dateContainer = document.createElement("div");
-  const dateLabel = document.createElement("label");
-  const dateInput = document.createElement("input");
-  const lowerCard = document.createElement("div");
-  const doneButton = document.createElement("button");
-  const removeButton = document.createElement("button");
+  const toDoCard = task1.render();
 
   // append to DOM and add classes
   // create todo card
   grid.appendChild(toDoCard);
-  toDoCard.classList.add("todo-card");
-
-  // create content within card
-  toDoCard.appendChild(toDoContent);
-  toDoContent.classList.add("todo-content");
-
-  // create the upper portion of the card
-  toDoContent.appendChild(upperCard);
-  upperCard.classList.add("upper-card");
-
-  upperCard.appendChild(theTitle);
-  upperCard.appendChild(theDescription);
-  upperCard.appendChild(theProject);
-  upperCard.appendChild(dateContainer);
-  dateContainer.classList.add("date-container");
-  dateContainer.appendChild(dateLabel);
-  dateContainer.appendChild(dateInput);
-
-  // create lower portion of the card
-  toDoContent.appendChild(lowerCard);
-  lowerCard.classList.add("lower-card");
-
-  lowerCard.appendChild(doneButton);
-  doneButton.classList.add("button");
-
-  lowerCard.appendChild(removeButton);
-  removeButton.classList.add("remove");
-
-  // add values to title, description, date, and buttons
-  theTitle.innerText = task1.title;
-  theDescription.innerText = `${task1.description}`;
-  theProject.innerText = task1.project;
-  dateLabel.innerText = "Due date:";
-  doneButton.innerText = "Done";
-  doneButton.value = `${task1.title}`;
-  dateInput.type = "date";
-  dateInput.value = task1.date;
-  dateInput.id = task1.title;
-  removeButton.innerText = "Remove";
-  removeButton.value = `${task1.title}`;
-
-  // add event listener to dynamic task card to complete it
-  doneButton.addEventListener("click", removeTask);
-
-  // add event listener to dynamic task card to remove it
-  removeButton.addEventListener("click", removeTask);
-
-  // add event listener for date input
-  dateInput.addEventListener("change", (e) => {
-    let updateTaskDate = myTasks.forEach((task) => {
-      if (e.target.id === task1.title) {
-        task1.date = e.target.value;
-      } else {
-      }
-    });
-  });
 
   // create the new project button if it does not already exist
   if (projectList.includes(task1.project) === false) {
--- src/task.js
+++ src/task.js
@@ -1,3 +1,5 @@
+import { removeTask } from './index.js';
+
 export class Task {
   constructor(title, description, project, date) {
     this.title = title;
@@ -8,6 +10,82 @@ export class Task {
   }
 
   info() {
-      return `${this.title}, ${this.description}, ${this.date}`;
+    return `${this.title}, ${this.description}, ${this.date}`;
+  }
+
+  render() {
+    // create elements
+    const toDoCard = document.createElement("div");
+    const toDoContent = document.createElement("div");
+    const upperCard = document.createElement("div");
+    const theTitle = document.createElement("h3");
+    const theDescription = document.createElement("p");
+    const theProject = document.createElement("p");
+    const dateContainer = document.createElement("div");
+    const dateLabel = document.createElement("label");
+    const dateInput = document.createElement("input");
+    const lowerCard = document.createElement("div");
+    const doneButton = document.createElement("button");
+    const removeButton = document.createElement("button");
+
+    toDoCard.classList.add("todo-card");
+
+    // create content within card
+    toDoCard.appendChild(toDoContent);
+    toDoContent.classList.add("todo-content");
+
+    // create the upper portion of the card
+    toDoContent.appendChild(upperCard);
+    upperCard.classList.add("upper-card");
+
+    upperCard.appendChild(theTitle);
+    upperCard.appendChild(theDescription);
+    upperCard.appendChild(theProject);
+    upperCard.appendChild(dateContainer);
+    dateContainer.classList.add("date-container");
+    dateContainer.appendChild(dateLabel);
+    dateContainer.appendChild(dateInput);
+
+    // create lower portion of the card
+    toDoContent.appendChild(lowerCard);
+    lowerCard.classList.add("lower-card");
+
+    lowerCard.appendChild(doneButton);
+    doneButton.classList.add("button");
+
+    lowerCard.appendChild(removeButton);
+    removeButton.classList.add("remove");
+
+    // add values to title, description, date, and buttons
+    theTitle.innerText = this.title;
+    theDescription.innerText = `${this.description}`;
+    theProject.innerText = this.project;
+    dateLabel.innerText = "Due date:";
+    doneButton.innerText = "Done";
+    doneButton.value = `${this.title}`;
+    dateInput.type = "date";
+    dateInput.value = this.date;
+    dateInput.id = this.title;
+    removeButton.innerText = "Remove";
+    removeButton.value = `${this.title}`;
+
+    // add event listener to dynamic task card to complete it
+    doneButton.addEventListener("click", removeTask);
+
+    // add event listener to dynamic task card to remove it
+    removeButton.addEventListener("click", removeTask);
+
+    // add event listener for date input
+    dateInput.addEventListener("change", (e) => {
+      let updateTaskDate = myTasks.forEach((task) => {
+        if (e.target.id === this.title) {
+          this.date = e.target.value;
+        } else {
+        }
+      });
+    });
+
+    return toDoCard;
   }
 }
+
